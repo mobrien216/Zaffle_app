9dde908aff532ae321fc75a15ac724f580f1045e
mobrien216 <mobrien@clevefdn.org>
2026-08-18 11:22:59 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ZaffleApp from "./App.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ZaffleApp />
-  </React.StrictMode>
-);
